d3761670014acfd1d52437d1467a62b2a4e17f0a
Yoshua Wuyts <yoshuawuyts+github@gmail.com>
2020-12-01 21:34:37 +0100

    aoc day 1

--- src/day1.rs
+++ src/day1.rs
@@ -4,8 +4,14 @@ use std::io::BufRead;
 const INPUT: &'static str = include_str!("../inputs/day1.txt");
 
 fn main() -> io::Result<()> {
-    for line in io::Cursor::new(INPUT).lines() {
-        dbg!(line?);
+    for (i, left) in io::Cursor::new(INPUT).lines().enumerate() {
+        let left: u64 = left?.parse().unwrap();
+        for right in io::Cursor::new(INPUT).lines().skip(i) {
+            let right: u64 = right?.parse().unwrap();
+            if left + right == 2020 {
+                println!("{}", left * right);
+            }
+        }
     }
     Ok(())
 }
